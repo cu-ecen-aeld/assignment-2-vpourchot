66de2332072e111d45daf970d2f8bbd1ea78e3ce
vpourchot <vpourchot@yahoo.com>
2023-01-25 22:15:27 -0500

    assignment 2

---
+++ assignments/assignment2/writer.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+//Setup syslog logging for your
+//utility using the LOG_USER facility
+
+int append_str(char *writefile,char *writestr);
+
+int main(int augc,char *argv[]) {
+	openlog(NULL,0,LOG_USER);
+	append_str(arg1,arg2);
+	puts("break0");
+	return 0;
+}
+
+int append_str(char *writefile,char *writestr) {
+	// Create file named $writestr
+	// Append 'Writing $writestr to file'
+	// to newly created file
+	// Write with LOG_DEBUG level
+	
+	puts("Break1");
+	
+	int fp = creat(writefile,0666);
+	if (fp == -1) {
+                syslog(LOG_ERR,"Directory does not exist: %s",writefile);
+		return 1;
+	}
+
+	ssize_t nr = write(fp,writestr,strlen(writestr));
+	if (nr == -1) {
+              syslog(LOG_ERR,"Invalid Number of arguments: No String Provided %s",writestr);
+		return 1;
+	}
+	int fsync (int fp);
+	puts("break2");
+	int close (int fp);
+	puts("break3");
+	syslog(LOG_DEBUG,"Writing %s to %s",writestr,writefile);
+	return 0;
+
+}
+
